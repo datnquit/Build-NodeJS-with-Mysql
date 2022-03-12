2bf131a9bfc4897f0a539866399bf170c01611e1
quang dat <datnquit@gmail.com>
2022-03-12 23:21:03 +0700

    update config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,8 @@
+require('dotenv/config');
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "nodejs_base"
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME
 }
